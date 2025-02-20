57868b38a6c87ba4c1886dc4afcbc923645145cb
Dennis Kemboi <denkogei11@gmail.com>
2025-02-20 18:41:13 +0300

    added home

---
+++ my-app/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptas voluptatem quos ipsum molestias officiis? Recusandae, ratione, blanditiis sequi iste itaque repudiandae aperiam dignissimos voluptatum atque aut id voluptates veritatis molestiae assumenda debitis officiis quaerat incidunt, perferendis in non esse? Deleniti adipisci veritatis reprehenderit quos assumenda, accusantium eaque ipsum eius, ducimus sunt amet nostrum similique culpa laborum sint iusto. Aliquid repellat inventore, minus ipsa minima magnam. Hic eveniet, aliquam eius voluptatum vero facere. Minima mollitia praesentium voluptatum nulla quibusdam voluptate adipisci molestiae officiis voluptates, voluptatem ratione temporibus, perferendis repellat distinctio tempore quisquam vitae ipsum rem earum. Aliquid placeat corporis odit!</h1>
+  </>
+  )
+}
+
+export default Home
